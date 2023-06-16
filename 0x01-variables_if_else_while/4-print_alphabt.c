fb4a36d5cc49685f76a105958eed3d382bfa388c
Micheal Sadek <michealsadek849@gmail.com>
2023-06-16 07:19:02 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description 'print alphabets except q and e'
+ * return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+	while (n <= 122)
+	{
+		if (n ==101 || n== 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
