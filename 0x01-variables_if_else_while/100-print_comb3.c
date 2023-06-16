10b2a1247dddcc8f8ece49bb8cb56c1241767371
Micheal Sadek <michealsadek849@gmail.com>
2023-06-16 10:37:39 -0700

    prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *main -Entry point
+ *main - Entry point
  *Description: 'prints all possible different combinations of two digits'
  *Return : Always 0
  */
