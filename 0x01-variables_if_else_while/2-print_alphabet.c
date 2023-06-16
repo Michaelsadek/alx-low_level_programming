27af2e65212df5ebc53ed2f7bf8c59bac6d9db7a
Micheal Sadek <michealsadek849@gmail.com>
2023-06-16 06:33:02 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: 'print alphabets lowrcase'
+ * return: always 0
+ */
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
