08325255c0656b956872985423075d39dc321da8
Micheal Sadek <michealsadek849@gmail.com>
2023-06-16 04:41:46 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,11 @@
+#include<stdib.h>
+#include<time.h>
+//more headers goes there
+//betty style foc for function main goes there
+int main (void)
+{
+	int x;
+		srand (time(0));
+		x =rand() - RAND_MAX / 2;
+		return (0);
+}
