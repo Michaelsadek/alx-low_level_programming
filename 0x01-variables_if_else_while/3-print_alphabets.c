63c7ee5d10eba1566cc32be88056994dc4a23358
Micheal Sadek <michealsadek849@gmail.com>
2023-06-16 06:43:19 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -entry point 
+ * description:'print alphabets lowercase'
+ * return:always 0
+ */
+int main(void)
+{
+	int n = 97;
+	while(n<= 122)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
