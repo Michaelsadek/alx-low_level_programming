653152a17ef0bbf53b38abba2db7f2f4c0ac326d
Micheal Sadek <michealsadek849@gmail.com>
2023-07-03 08:07:45 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - entry point
+ * @n: imput
+ * @dest: input
+ * @src: input
+ * Return: Always 0 (success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i< n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
