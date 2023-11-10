f7b9c001a08d914610a5f9c22a9179664f25100e
Michaelsadek <michealsadek849@gmail.com>
2023-11-09 17:30:45 -0800

    a

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * distint_len - returns length of dist
+ * @h: address of head node
+ *
+ * Return: size of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
